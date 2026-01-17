d9dd6c571261a93327b8ab2a7f042b9679c2965b
Nehuen Lian <nehuenn.lian@gmail.com>
2026-01-17 01:59:03 -0300

    assert errors reading the UI message and assert successes reading memory database

--- tests/integration/test_manage_prices.py
+++ tests/integration/test_manage_prices.py
@@ -1,13 +1,19 @@
 from unittest.mock import patch
 
+from sqlalchemy import select
+
 from src.business_logic.manage_prices import PriceManagement
 from src.controllers.manage_prices import PricesManagementController
+from src.data_access.database_tables import Stock
 
-from ..conftest import mock_session_scope
+from ..conftest import get_memory_session, mock_session_scope
 from ..views.fake_views import FakePriceViewManager
 
 
 def test_manage_prices_success(insert_data_in_memory_db):
+
+    BARCODE = "7790895000997"
+
     fake_view = FakePriceViewManager()
     manage_prices_business = PriceManagement()
     
@@ -15,8 +21,14 @@ def test_manage_prices_success(insert_data_in_memory_db):
     prices_mgmt.view = fake_view
 
     with patch("src.business_logic.manage_prices.session_scope", mock_session_scope):
-        prices_mgmt.get_product("7790895000997")
+        prices_mgmt.get_product(BARCODE)
         prices_mgmt.update_price(1, 4000.00)
+
+    # verify price has changed:
+    session = get_memory_session()
+    product = session.execute(select(Stock).filter_by(db_barcode=BARCODE)).scalar_one()
+    assert product.db_final_price_to_consumer == 4000.00
+
 
 def test_manage_prices_product_not_found():
     fake_view = FakePriceViewManager()
@@ -26,7 +38,9 @@ def test_manage_prices_product_not_found():
     prices_mgmt.view = fake_view
 
     with patch("src.business_logic.manage_prices.session_scope", mock_session_scope):
-        prices_mgmt.get_product("unknown barcode")
+        prices_mgmt.get_product("1111111111")
+
+    assert fake_view.notifications_list[0] == "Producto no encontrado."
 
 def test_manage_prices_invalid_price():
     fake_view = FakePriceViewManager()
@@ -38,3 +52,5 @@ def test_manage_prices_invalid_price():
     with patch("src.business_logic.manage_prices.session_scope", mock_session_scope):
         prices_mgmt.get_product("7790895000997")
         prices_mgmt.update_price(1, -1)
+
+    assert fake_view.notifications_list[0] == "El nuevo precio no puede ser negativo o menor a $1."
--- tests/integration/test_register_sale.py
+++ tests/integration/test_register_sale.py
@@ -1,10 +1,14 @@
 from unittest.mock import patch
 
+from sqlalchemy import select
+
 from src.business_logic.register_sale import SaleManagement
 from src.controllers.register_sale import SalesManagementController
+from src.data_access.database_tables import Sales
 
-from ..conftest import mock_session_scope
+from ..conftest import get_memory_session, mock_session_scope
 from ..views.fake_views import FakeSalesViewManager
+
 
 def thread_mock(facturation_handler):
     return facturation_handler()
@@ -28,3 +32,36 @@ def test_register_sale_success(insert_data_in_memory_db):
 
         with patch("src.controllers.register_sale.use_other_thread", thread_mock):
             register_sale_mgmt.complete_sale()
+
+    # verify sale success:
+    session = get_memory_session()
+    sale = session.execute(select(Sales).filter_by(id=1)).scalar_one()
+    assert sale.id == 1
+
+
+def test_register_sale_invalid_barcode():
+    fake_view = FakeSalesViewManager()
+    sales_management_business = SaleManagement()
+
+    register_sale_mgmt = SalesManagementController(sales_management_business)
+    register_sale_mgmt.view = fake_view
+
+    with patch("src.business_logic.register_sale.session_scope", mock_session_scope):
+
+        register_sale_mgmt.get_product("''")
+
+    assert fake_view.notifications_list[0] == "El código de barras contiene caracteres inválidos."
+
+
+def test_register_sale_product_not_found():
+    fake_view = FakeSalesViewManager()
+    sales_management_business = SaleManagement()
+
+    register_sale_mgmt = SalesManagementController(sales_management_business)
+    register_sale_mgmt.view = fake_view
+
+    with patch("src.business_logic.register_sale.session_scope", mock_session_scope):
+
+        register_sale_mgmt.get_product("1111111111")
+
+    assert fake_view.notifications_list[0] == "Producto no encontrado."
