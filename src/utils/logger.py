428cf34c7170bbe11a0b7db2688e2d61cfc52ef1
Nehuen Lian <nehuenn.lian@gmail.com>
2026-01-19 05:25:44 -0300

    adapt log paths for dev and exe environments

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,14 +1,40 @@
 import logging
+import os
+import sys
 from logging.handlers import TimedRotatingFileHandler
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 
+def get_logs_path():
+    if getattr(sys, 'frozen', False):
+        base_dir = os.path.dirname(sys.executable)
+        logs_dir = os.path.join(base_dir, "logs")
+
+    else:
+        base_dir = os.path.abspath(".")
+        logs_dir = os.path.join(base_dir, "src", "logs")
+
+    return logs_dir
+
+logs_dir = get_logs_path()
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+
+
+# ============================
+# ======== SETUP LOGS ======== 
+# ============================
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
-business_file_handler = TimedRotatingFileHandler('src/logs/business.log', when='midnight', interval=1, backupCount=7)
-data_access_files_handler = TimedRotatingFileHandler('src/logs/data_access.log', when='midnight', interval=1, backupCount=7)
-controller_file_handler = TimedRotatingFileHandler('src/logs/controller.log', when='midnight', interval=1, backupCount=7)
+business_logs_path = os.path.join(logs_dir, 'business.log')
+data_access_logs_path = os.path.join(logs_dir, 'data_access.log')
+controller_logs_path = os.path.join(logs_dir, 'controller.log')
+
+business_file_handler = TimedRotatingFileHandler(business_logs_path, when='midnight', interval=1, backupCount=7)
+data_access_files_handler = TimedRotatingFileHandler(data_access_logs_path, when='midnight', interval=1, backupCount=7)
+controller_file_handler = TimedRotatingFileHandler(controller_logs_path, when='midnight', interval=1, backupCount=7)
 
 business_file_handler.setFormatter(formatter)
 data_access_files_handler.setFormatter(formatter)
@@ -18,16 +44,13 @@ console_logger = logging.getLogger('console')
 console_logger.addHandler(console_handler)
 console_logger.setLevel(logging.DEBUG)
 
-
 business_logger = logging.getLogger('business')
 business_logger.addHandler(business_file_handler)
 business_logger.setLevel(logging.DEBUG)
 
-
 data_access_logger = logging.getLogger('data_access')
 data_access_logger.addHandler(data_access_files_handler)
 data_access_logger.setLevel(logging.WARNING)
-
 
 controller_logger = logging.getLogger('controller')
 controller_logger.addHandler(controller_file_handler)
