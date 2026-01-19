0f4672e6c6c42dee61ab1b6bd8286df993a3f065
Nehuen Lian <nehuenn.lian@gmail.com>
2026-01-18 21:26:03 -0300

    add a generic function that resolve paths

---
+++ src/utils/settings.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+
+def resource_path(relative_path):
+    """
+    Gets the absolute path to the resource, works for development and PyInstaller
+    """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
